b81ecbaa91a3fbc49693f3ec4ee1e00052dfd2e4
pavlvs <pavlvsxavier@gmail.com>
2021-09-08 18:36:45 +0100

    displayed both movie summaries

--- index.js
+++ index.js
@@ -48,10 +48,7 @@ const autoCompleteConfig = {
             ${movie.Title}
         `
     },
-    onOptionSelect(movie) {
-        document.querySelector('.tutorial').classList.add('is-hidden')
-        onMovieSelect(movie)
-    },
+
     inputValue(movie) {
         return movie.Title
     },
@@ -73,13 +70,21 @@ const autoCompleteConfig = {
 createAutoComplete({
     ...autoCompleteConfig,
     root: document.querySelector('#left-autocomplete'),
+    onOptionSelect(movie) {
+        document.querySelector('.tutorial').classList.add('is-hidden')
+        onMovieSelect(movie, document.querySelector('#left-summary'))
+    },
 })
 createAutoComplete({
     ...autoCompleteConfig,
     root: document.querySelector('#right-autocomplete'),
+    onOptionSelect(movie) {
+        document.querySelector('.tutorial').classList.add('is-hidden')
+        onMovieSelect(movie, document.querySelector('#right-summary'))
+    },
 })
 
-const onMovieSelect = async (movie) => {
+const onMovieSelect = async (movie, summaryElement) => {
     const response = await axios.get('http://www.omdbapi.com/?', {
         params: {
             apikey: 'e1826389',
@@ -87,7 +92,7 @@ const onMovieSelect = async (movie) => {
         },
     })
 
-    document.querySelector('#summary').innerHTML = movieTemplate(response.data)
+    summaryElement.innerHTML = movieTemplate(response.data)
 }
 
 const movieTemplate = (movieDetail) => {
