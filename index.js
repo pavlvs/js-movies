4f356bb6560805c39427109a2fe7c6b267b51d16
pavlvs <pavlvsxavier@gmail.com>
2021-09-09 08:42:56 +0100

    compared left and right summaries stats and styled according to which one is greater

--- index.js
+++ index.js
@@ -111,7 +111,28 @@ const onMovieSelect = async (movie, summaryElement, side) => {
 }
 
 const runComparison = () => {
-    console.log('Time for comparison')
+    const leftSideStats = Array.from(
+        document.querySelectorAll('#left-summary .notification')
+    )
+
+    const rightSideStats = Array.from(
+        document.querySelectorAll('#right-summary .notification')
+    )
+
+    leftSideStats.forEach((leftStat, index) => {
+        const rightStat = rightSideStats[index]
+
+        const leftSideValue = parseInt(leftStat.dataset.value)
+        const rightSideValue = parseInt(rightStat.dataset.value)
+
+        if (rightSideValue > leftSideValue) {
+            leftStat.classList.remove('is-primary')
+            leftStat.classList.add('is-warning')
+        } else {
+            rightStat.classList.remove('is-primary')
+            rightStat.classList.add('is-warning')
+        }
+    })
 }
 
 const movieTemplate = (movieDetail) => {
